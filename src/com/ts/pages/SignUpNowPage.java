b3df65fd529fe8be4d24e7bf10cf345ae3f477d3
Vidhya Kasiviswanathan <vidhya.kasiviswanathan@gmail.com>
2016-05-10 16:11:50 -0700

    Added tests for Confirmation Password

--- src/com/ts/pages/SignUpNowPage.java
+++ src/com/ts/pages/SignUpNowPage.java
@@ -3,10 +3,12 @@ package com.ts.pages;
 import com.ts.base.BasePage;
 import com.ts.utils.web.PageDriver;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 
 /**
  * Created by Vidhya Kasiviswanathan on 5/5/2016.
- * Updated with visibilityWait() by Vidhya Kasiviswanathan oon 5/9/2016.
+ * Updated based on locators.json signup.invaliddetails by Vidhya Kasiviswanathan on 5/10/2016
+ * Added tests for Confirmation password by Vidhya Kasiviswanathan on 5/10/2016
  */
 
 public class SignUpNowPage extends BasePage {
@@ -70,9 +72,8 @@ public class SignUpNowPage extends BasePage {
     public String invalidEmailAddressFormat(String email) {
         driver.findElement("id=signup.email").clear();
         driver.findElement("id=signup.email").sendKeys(email);
-        driver.visibilityWait("signup.invalidemail");
-        driver.visibilityWait("signup.invalidemail");
-        return driver.findElement("signup.invalidemail").getText();
+        driver.visibilityWait("signup.invaliddetails");
+        return driver.findElement("signup.invaliddetails").getText();
     }
 
     //Check Email Required error message
@@ -80,25 +81,81 @@ public class SignUpNowPage extends BasePage {
         driver.findElement("id=signup.email").clear();
         driver.findElement("id=signup.email").sendKeys("x");
         driver.findElement("id=signup.email").pressBackspace();
-        driver.visibilityWait("signup.invalidemail");
-        return driver.findElement("signup.invalidemail").getText();
+        driver.visibilityWait("signup.invaliddetails");
+        return driver.findElement("signup.invaliddetails").getText();
     }
 
 
     //Check Password Must be 8-20 characters error message
     public String invalidPasswordChars(String password) {
+        driver.findElement("id=signup.email").clear();
+        driver.findElement("id=signup.email").sendKeys("abc@gmail.com");
         driver.findElement("id=signup.epassword").clear();
         driver.findElement("id=signup.epassword").sendKeys(password);
-        driver.visibilityWait("signup.invalidpwdchars");
-        return driver.findElement("signup.invalidpwdchars").getText();
+        driver.visibilityWait("signup.invaliddetails");
+        return driver.findElement("signup.invaliddetails").getText();
     }
 
     //Check Password Required error message
     public String backspacePassword(String password) {
+        driver.findElement("id=signup.email").clear();
+        driver.findElement("id=signup.email").sendKeys("abc@gmail.com");
         driver.findElement("id=signup.epassword").clear();
         driver.findElement("id=signup.epassword").sendKeys(password);
         driver.findElement("id=signup.epassword").pressBackspace();
-        return driver.findElement("signup.pwdrequired").getText();
+        driver.visibilityWait("signup.invaliddetails");
+        return driver.findElement("signup.invaliddetails").getText();
     }
+
+    //Check Password criteria
+    public String invalidPassword(String password) {
+        driver.findElement("id=signup.email").clear();
+        driver.findElement("id=signup.email").sendKeys("abc@gmail.com");
+        driver.findElement("id=signup.epassword").clear();
+        driver.findElement("id=signup.epassword").sendKeys(password);
+        driver.visibilityWait("signup.invaliddetails");
+        return driver.findElement("signup.invaliddetails").getText();
+    }
+
+    //Check Confirmation Password don't match
+    public String confirmPasswordsDontMatch(String password) {
+        driver.findElement("id=signup.email").clear();
+        driver.findElement("id=signup.email").sendKeys("abc@gmail.com");
+        driver.findElement("id=signup.epassword").clear();
+        driver.findElement("id=signup.epassword").sendKeys("Abc12345");
+        driver.findElement("id=signup.mpassword").clear();
+        driver.findElement("id=signup.mpassword").sendKeys(password);
+        driver.visibilityWait("signup.invaliddetails");
+        return driver.findElement("signup.invaliddetails").getText();
+    }
+
+    //Check Confirmation Password Required
+    public String backspaceConfirmPassword(String password) {
+        driver.findElement("id=signup.email").clear();
+        driver.findElement("id=signup.email").sendKeys("abc@gmail.com");
+        driver.findElement("id=signup.epassword").clear();
+        driver.findElement("id=signup.epassword").sendKeys("Abc12345");
+        driver.findElement("id=signup.mpassword").clear();
+        driver.findElement("id=signup.mpassword").sendKeys(password);
+        driver.findElement("id=signup.mpassword").pressBackspace();
+        driver.visibilityWait("signup.invaliddetails");
+        return driver.findElement("signup.invaliddetails").getText();
+    }
+
+    //Success Create Account
+    //CREATE AN ACCOUNT button is currently not working
+    //Once it works, need to update test for email address already exists scenario
+    public String createAccountSuccess() {
+        driver.findElement("id=signup.email").clear();
+        driver.findElement("id=signup.email").sendKeys("abc@gmail.com");
+        driver.findElement("id=signup.epassword").clear();
+        driver.findElement("id=signup.epassword").sendKeys("Abc12345");
+        driver.findElement("id=signup.mpassword").clear();
+        driver.findElement("id=signup.mpassword").sendKeys("Abc12345");
+        driver.findElement("signup.createaccountbtn").click();
+        return driver.getCurrentUrl();
+    }
+
+
 
 }
